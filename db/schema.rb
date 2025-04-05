f480af9b0a408041e4ebf49bdf73fcd7fa027ff2
kimu805 <kimukimuok805@gmail.com>
2025-04-05 18:57:08 +0900

    レシピのコントローラを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_05_084126) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_05_094214) do
+  create_table "recipes", charset: "utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.text "ingredients"
+    t.text "steps"
+    t.integer "cooking_time"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -24,4 +36,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_05_084126) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "recipes", "users"
 end
