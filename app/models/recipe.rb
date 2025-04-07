2eeb8037f5dc0e3d212626d1f27b4253757bc8f5
kimu805 <kimukimuok805@gmail.com>
2025-04-07 10:39:40 +0900

    Update recipe.rb

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,16 @@
 class Recipe < ApplicationRecord
+  # association
   belongs_to :user
+
+  # validation
+  validates :title,
+    presence: true,
+    length: { minimum: 1, maximum: 50 }
+  with_options length: { maximum: 1000 } do
+    validates :description
+    validates :ingredients
+    validates :steps
+  end
+  validates :cooking_time,
+    numericality: { only_integer: true }
 end
